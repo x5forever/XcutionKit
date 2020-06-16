2ef76e408ebe0d7db6e7c91681ed9e7af968b1f2
x5 <wuxu1@creditease.cn>
2020-06-16 12:58:30 +0800

    no message

--- XcutionDemo/Pods/SVTransport/SVRequestDemo/SVRequestDemo/SVTransport.framework/Headers/SVTransport.h
+++ XcutionDemo/Pods/SVTransport/SVRequestDemo/SVRequestDemo/SVTransport.framework/Headers/SVTransport.h
@@ -3,7 +3,7 @@
 //
 //  Created by x5 on 2018/7/7.
 //  Copyright © 2018年 x5. All rights reserved.
-//  V2.2.0 update 2/21/2020
+//  V2.2.1 update 5/29/2020
 
 #import <Foundation/Foundation.h>
 
@@ -14,6 +14,6 @@ typedef void (^SVTransportBlock)(NSDictionary * _Nullable result, NSError * _Nul
 + (void)setAppId:(NSString * _Nonnull)appId appKey:(NSString * _Nonnull)appKey;
 + (void)transportWithBlock:(SVTransportBlock _Nullable)block;
 + (void)transportWithPath:(NSString *_Nonnull)path bodyParams:(NSDictionary *_Nullable)bodyParams block:(SVTransportBlock _Nullable)block;
-+ (void)transportWithBaseUrl:(NSString *_Nullable)url path:(NSString *_Nonnull)path bodyParams:(NSDictionary *_Nullable)bodyParams block:(SVTransportBlock _Nullable)block;
++ (void)transportWithUrl:(NSString *_Nullable)url path:(NSString *_Nonnull)path bodyParams:(NSDictionary *_Nullable)bodyParams block:(SVTransportBlock _Nullable)block;
 
 @end
